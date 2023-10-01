fb4ed4781713ec16f6cf644606113840d99af7d9
mghijs <michael.ghijs@ugent.be>
2023-10-01 16:04:51 +0200

    Tutorial part  one

--- app/app.py
+++ app/app.py
@@ -1,38 +1,131 @@
-"""dashtest
-
-just a test
-"""
-
-import dash
-from dash import Input, Output, State
-
-from dashtest.layout import THEME
-from dashtest.utils import generate_layout
-
-# ----------------- Initialize App --------------------------
-
-app = dash.Dash(__name__, external_stylesheets=[THEME], use_pages=True, pages_folder="")
-app.title = "dashtest"
-server = app.server
-
-# ----------------- Page Registry ---------------------------
-
-dash.register_page("dashtest", path="/", layout=generate_layout())
-
-# ----------------- Layout ----------------------------------
-
-app.layout = dash.page_container
-
-# ------------------ Callbacks ------------------------------
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
 
 
-@app.callback(
-    Output("modal", "is_open"),
-    [Input("dropdown-button", "n_clicks"), Input("close-button", "n_clicks")],
-    [State("modal", "is_open")],
-)  # type: ignore[misc]
-def toggle_modal(n1: int, n2: int, is_open: bool) -> bool:
-    """Toggle the visibility of the 'About' modal."""
-    if n1 or n2:
-        return not is_open
-    return is_open
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+# table demo
+df_agri_export = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+# figure plotly demo
+
+df_gdp = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+
+fig_gdp = px.scatter(df_gdp, x="gdp per capita", y="life expectancy",
+                 size="population", color="continent", hover_name="country",
+                 log_x=True, size_max=60)
+
+
+# markdown text
+
+markdown_text = '''
+### Dash and Markdown
+
+Dash apps can be written in Markdown.
+Dash uses the [CommonMark](http://commonmark.org/)
+specification of Markdown.
+Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
+if this is your first introduction to Markdown!
+'''
+
+app = Dash(__name__)
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montfake", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+for fg in [fig, fig_gdp]:
+    fg.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+    )
+
+app.layout = html.Div(style={'backgroundColor': colors['background'],
+                             'textAlign': 'center','display': 'flex', 'flex-direction': 'column',
+            'color': colors['text']}, 
+            children=[
+    html.H1(
+        children='Hello Dash',
+    ),
+
+    html.Div(children='Dash: A web application framework for your data.'),
+    dcc.Markdown(children=markdown_text),
+    
+    html.Div(children=[
+        html.Label('Dropdown'),
+        dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'], 'Montréal'),
+
+        html.Br(),
+        html.Label('Multi-Select Dropdown'),
+        dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'],
+                     ['Montréal', 'San Francisco'],
+                     multi=True),
+
+        html.Br(),
+        html.Label('Radio Items'),
+        dcc.RadioItems(['New York City', 'Montréal', 'San Francisco'], 'Montréal'),
+    ], style={'padding': 10, 'flex': 1}),
+
+    html.Div(children=[
+        html.Label('Checkboxes'),
+        dcc.Checklist(['New York City', 'Montréal', 'San Francisco'],
+                      ['Montréal', 'San Francisco']
+        ),
+
+        html.Br(),
+        html.Label('Text Input'),
+        dcc.Input(value='MTL', type='text'),
+
+        html.Br(),
+        html.Label('Slider'),
+        dcc.Slider(
+            min=0,
+            max=9,
+            marks={i: f'Label {i}' if i == 1 else str(i) for i in range(1, 6)},
+            value=5,
+        ),
+    ], style={'padding': 10, 'flex': 1}),
+
+    dcc.Graph(
+        id='example-graph-2',
+        figure=fig
+    ),
+    html.Div(children=[
+    html.H3(children='US Agriculture Exports (2011)'),
+    generate_table(df_agri_export)]), 
+    html.H3("Nice plotly GDP plot"),
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure=fig_gdp
+    )
+],)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
